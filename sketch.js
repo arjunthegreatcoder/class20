50da5b74db1ae74e416bb0d405515a0a7770f081
arjunthegreatcoder <73948843+arjunthegreatcoder@users.noreply.github.com>
2020-11-24 21:00:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect = createSprite(400, 200, 100, 50);
+  movingRect = createSprite(200, 100, 50, 100);
+
+  fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+}
+
+function draw() {
+  background("black");  
+  movingRect.x = mouseX;
+  movingRect.y = mouseY;
+
+  console.log(movingRect.x - fixedRect.x);
+
+  if(movingRect.x - fixedRect.x <= fixedRect.width/2 + movingRect.width/2 &&
+    fixedRect.x - movingRect.x <= fixedRect.width/2 + movingRect.width/2 &&
+    movingRect.y-fixedRect.y<=fixedRect.height/2+movingRect.height/2&&
+    fixedRect.y-movingRect.y<=fixedRect.height/2+movingRect.height/2){
+    fixedRect.shapeColor = "red"; 
+  movingRect.shapeColor = "red";
+  }
+  else{
+    fixedRect.shapeColor = "green";
+  movingRect.shapeColor = "green";
+  }
+
+  drawSprites();
+}
